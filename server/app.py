1c60b9dd3f20385579adddf54d3d952fdd01e5e4
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-11-12 14:24:34 +0000

    register and login working

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 import bcrypt
+from flask_bcrypt import Bcrypt
 import json
 from bson import json_util
 # from auth.auth import create_access_token
@@ -19,19 +20,21 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode["exp"] = expire
-    return jwt.encode(to_encode, secret_key, algorithm=algorithm) 
+    return jwt.encode(to_encode, secret_key, algorithm=algorithm)
+
 
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
+
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
 
 app.config['MONGO_DBNAME'] = 'selectric'
 app.config['MONGO_URI'] = 'mongodb+srv://dimeji:kushalpatel@cluster0.tbprf.mongodb.net/selectric?ssl=true&ssl_cert_reqs=CERT_NONE'
-mongo= PyMongo(app)
+mongo = PyMongo(app)
 secret = 'brodie-secret'
-
-
 
 
 # login post route
@@ -43,33 +46,33 @@ def login():
     print(data)
     print(email)
     found_user = mongo.db.selectric.find_one({"email": f'{email}'})
-    if found_user and bcrypt.checkpw(str(password.encode('utf8')),found_user['password'].encode('utf8')):
-        token = create_access_token(parse_json(found_user))
+    if found_user and bcrypt.check_password_hash(found_user['password'], password):
+        token = create_access_token(data=parse_json(found_user))
         return parse_json(token), 200
 
 # register post route
+
+
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.get_json() #request.json
+    data = request.get_json()  # request.json
     email = data['email']
     username = data['username']
     password = data['password']
-    
-    hashed_pass = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-    
+
+    hashed_pass = bcrypt.generate_password_hash(password).decode('utf8')
+
     new_user = {'email': email, 'username': username, 'password': hashed_pass}
     mongo.db.selectric.save(new_user)
     token = create_access_token(data=parse_json(new_user))
     print(token)
     return parse_json(token), 201
-    
 
 
 # get user by id
-@app.route('/user', methods=['POST','PATCH', 'DELETE'])
+@app.route('/user', methods=['POST', 'PATCH', 'DELETE'])
 def user():
     return
-
 
 
 if __name__ == '__main__':
